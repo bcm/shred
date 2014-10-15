70b255456d1b9524ca5e8ae29e4febe87f4ad81f
Brian Moseley <bcm@maz.org>
2014-10-15 10:59:15 -0700

    support multiple s3 buckets

--- lib/shred/commands/s3.rb
+++ lib/shred/commands/s3.rb
@@ -5,34 +5,25 @@ require 'shred/commands/base'
 module Shred
   module Commands
     class S3 < Base
-      desc 'mkbucket', 'Create an S3 bucket'
+      desc 'mkbucket NAME REGION', 'Create an S3 bucket'
       long_desc <<-LONGDESC
-        Create an S3 bucket.
+        Create an S3 bucket with the given name in the given region.
 
-        The bucket is created in the region identified by the `region` option or by
-        the `commands.s3.bucket.region` config var. If the value of the option or config
-        var is prefixed with `dotenv:` then the remaining portion of the value is
-        interpreted as a key in the user's .env file whose value is the region in which
-        the bucket is to be created.
+        If the name is prefixed with `dotenv:` then the remaining portion of the value is interpreted as a key in the
+        user's .env file whose value is the name of the bucket.
 
-        The bucket's name is specified by the `name` option or by the
-        `commands.s3.bucket.name` config var. If the value of the option or config var is
-        prefixed with `dotenv:` then the remaining portion of the value is interpreted
-        as a key in the user's .env file whose value is the name of the bucket.
+        If the region is prefixed with `dotenv:` then the remaining portion of the value is interpreted as a key in
+        the user's .env file whose value is the region in which the bucket is to be created.
       LONGDESC
-      option :name
-      option :region
-      def mkbucket
+      def mkbucket(name, region)
         ::Dotenv.load
 
-        region = options[:region] || cfg('bucket.region')
-        if region =~ /^dotenv:(.+)$/
-          region = ENV[$1] or raise "$1 not found in .env"
-        end
-
-        name = options[:name] || cfg('bucket.name')
         if name =~ /^dotenv:(.+)$/
           name = ENV[$1] or raise "$1 not found in .env"
+        end
+
+        if region =~ /^dotenv:(.+)$/
+          region = ENV[$1] or raise "$1 not found in .env"
         end
 
         s3 = AWS::S3.new(region: region)
@@ -41,6 +32,40 @@ module Shred
         else
           s3.buckets.create(name)
           console.say_ok("Created S3 bucket #{name} in region #{region}")
+        end
+      end
+
+      desc 'mkbuckets', 'Create all configured S3 buckets'
+      long_desc <<-LONGDESC
+        Create an S3 bucket for each element of the `commands.s3.buckets` config var.
+
+        If a bucket name is prefixed with `dotenv:` then the remaining portion of the value is interpreted as a key in
+        the user's .env file whose value is the name of the bucket.
+
+        If a bucket region is prefixed with `dotenv:` then the remaining portion of the value is interpreted as a key
+        in the user's .env file whose value is the region in which the bucket is to be created.
+      LONGDESC
+      def mkbuckets
+        ::Dotenv.load
+
+        cfg('buckets').each do |(key, bucket_cfg)|
+          name = bucket_cfg['name']
+          if name =~ /^dotenv:(.+)$/
+            name = ENV[$1] or raise "$1 not found in .env"
+          end
+
+          region = bucket_cfg['region']
+          if region =~ /^dotenv:(.+)$/
+            region = ENV[$1] or raise "$1 not found in .env"
+          end
+
+          s3 = AWS::S3.new(region: region)
+          if s3.buckets[name].exists?
+            console.say_ok("S3 bucket #{name} already exists in region #{region}")
+          else
+            s3.buckets.create(name)
+            console.say_ok("Created S3 bucket #{name} in region #{region}")
+          end
         end
       end
     end
